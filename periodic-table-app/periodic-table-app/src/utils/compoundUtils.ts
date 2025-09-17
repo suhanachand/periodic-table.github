e5946169b8c4e1cfb768bd8d4b46bf1d2a8ae29f
suhanachand <suhanachand03@gmail.com>
2025-09-16 20:34:41 -0700

    Delete periodic-table-app directory

--- periodic-table-app/periodic-table-app/src/utils/compoundUtils.ts
+++
@@ -1,8 +0,0 @@
-export const calculateAtomicMass = (elements: { atomicMass: number; quantity: number }[]): number => {
-    return elements.reduce((total, element) => total + element.atomicMass * element.quantity, 0);
-};
-
-export const generateCompoundNames = (elements: { name: string; quantity: number }[]): string[] => {
-    const names = elements.map(element => `${element.name}${element.quantity > 1 ? element.quantity : ''}`);
-    return names.length > 1 ? [names.join('-')] : names;
-};
